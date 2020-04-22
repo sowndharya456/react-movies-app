4887333fb15bf0dbda4bf38fb18329a349330387
sowndharya456 <sowndharyar12@gmail.com>
2020-04-23 00:03:39 +0530

    Added Tabs inside modal for Login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,35 +3,50 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 class Header extends Component {
-     constructor(){
-         super();
-         this.state={
-             modalIsOpen:false
-         };
-     }
+    constructor() {
+        super();
+        this.state = {
+            modalIsOpen: false,
+            value:0
+        };
+    }
 
-     openModalHandler = () => {
-         this.setState({
-             modalIsOpen:true});
-     }
-
-     closeModalHandler = () => {
+    openModalHandler = () => {
         this.setState({
-            modalIsOpen:false});
-     }
+            modalIsOpen: true
+        });
+    }
+
+    closeModalHandler = () => {
+        this.setState({
+            modalIsOpen: false
+        });
+    }
+    tabChangeHandler = (event , value) => {
+          this.setState({value});
+    }
+
     render() {
 
         return (
             <div>
                 <header className="app-header">
-                    <img src={logo} className="app-logo" alt="Movies App Logo"/>
+                    <img src={logo} className="app-logo" alt="Movies App Logo" />
                     <div className="login-button">
                         <Button variant="contained" color="default" onClick={this.openModalHandler}>Login</Button>
                     </div>
                 </header>
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}></Modal>
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+                <Tabs value={this.state.value} onChange={this.tabChangeHandler} >
+                    <Tab label="Login"></Tab>
+                    <Tab label="Register"></Tab>
+                </Tabs>
+                </Modal>
+                
             </div>
         )
     }
