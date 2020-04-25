6bb7eb66c8756c6eb9b59f88fd4e4c0e7781da24
sowndharya456 <sowndharyar12@gmail.com>
2020-04-25 16:16:18 +0530

    Added grid list for ‘Upcoming Movies

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,24 +2,29 @@ import React, { Component } from "react";
 import "./Home.css";
 import Header from "../../common/header/Header";
 import { withStyles } from '@material-ui/core/styles'
-//import moviesData from '../../common/movieData'; 
+import moviesData from '../../common/movieData'; 
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+
+
 
 const styles = theme => ({
-    // root: {
-    //     flexGrow: 1,
-    //     backgroundColor: theme.palette.background.paper
-    // },
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
     upcomingMoviesHeading: {
         textAlign: 'center',
         background: '#ff9999',
         padding: '8px',
         fontSize: '1rem'
     },
-    // gridListUpcomingMovies: {
-    //     flexWrap: 'nowrap',
-    //     transform: 'translateZ(0)',
-    //     width: '100%'
-    // }
+    gridListUpcomingMovies: {
+        flexWrap: 'nowrap',
+        transform: 'translateZ(0)',
+        width: '100%'
+    }
 });
 class Home extends Component {
     render() {
@@ -29,7 +34,18 @@ class Home extends Component {
                 <Header />
                 <div className={classes.upcomingMoviesHeading}>
                     <span>Upcoming Movies</span>
-                </div></div>
+                </div>
+                <GridList cols="5" className={classes.gridListUpcomingMovies}>
+                    {moviesData.map(movie => (
+                        
+                    <GridListTile key={movie.id}>
+                        <img src={movie.poster_url} alt={movie.title}/>
+                        <GridListTileBar title={movie.title}></GridListTileBar>
+                    </GridListTile>
+
+                    ))}
+                </GridList>
+                </div>
         );
     }
 }
