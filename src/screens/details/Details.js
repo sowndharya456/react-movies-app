9f1fff52c148725acbc1c5743572dac79b3a1f7b
sowndharya456 <sowndharyar12@gmail.com>
2020-04-27 19:46:45 +0530

    Added ‘Back to Home’ button and YouTube trailer on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -3,7 +3,9 @@ import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
 import Typography from '@material-ui/core/Typography';
 import './Details.css';
-
+import Home from '../../screens/home/Home';
+import ReactDOM from 'react-dom';
+import YouTube from 'react-youtube';
 
 class Details extends Component {
     constructor() {
@@ -18,13 +20,34 @@ class Details extends Component {
             return mov.id === this.props.movieId
         })[0];
         this.setState({ currentState });
-
-
+   
     }
+    backToHomeHandler = () =>{
+
+        ReactDOM.render(<Home />, document.getElementById('root'));
+    }
+    
+    _onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    }
+
     render() {
         let movie = this.state.movie;
+        console.log(movie);
+        const opts={
+        height: '300',
+            width: '700',
+            playerVars: {
+                autoplay: 1
+            }
+        }
         return (<div className='details'>
             <Header />
+            <div className="back" onClick={this.backToHomeHandler}>
+                <Typography>&#60;Back to Home</Typography>
+
+            </div>
             <div className="flex-containerDetails">
                 <div className="leftDetails">
                     <img src={movie.poster_url} alt={movie.title} />;
@@ -54,8 +77,20 @@ class Details extends Component {
                     <div className="marginTop16">
                         <Typography ><span className="bold" >Plot :</span><a href={movie.wiki_url}>(Wiki Link)</a>{movie.storyline} </Typography>
                     </div>
-                    <br />
-                </div>
+                    <div className="trailerContainer">
+                            <Typography>
+                                <span className="bold">Trailer:</span>
+                            </Typography>
+                           {console.log("movieURL:",this.state.movie.trailer_url)}
+                           { this.state.movie.trailer_url !== undefined &&  
+                            <YouTube  
+                                videoId={movie.trailer_url.split("?v=")[1]}
+                                opts={opts}
+                                onReady={this._onReady}
+                            />
+                           }
+                        </div>
+                </div >
                 <div className="rightDetails"></div>
             </div>
         </div>);
